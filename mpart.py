1441ab1905f1eb94c9d2949c7107ac04fa110e22
baris-saltik <baris.saltik@gmail.com>
2025-02-21 19:43:17 +0400

    added time-based aborts

--- mpart.py
+++ mpart.py
@@ -1,4 +1,4 @@
-import pathlib, boto3, os, datetime, pprint, random
+import pathlib, boto3, os, datetime, pprint, random, pytz
 from yaml import load, dump, Loader, Dumper
 from boto3.session import Session,Config
 
@@ -7,6 +7,7 @@ configFile = os.path.join(pathlib.Path(__file__).resolve().parents[0], "config",
 with open(configFile, mode="r") as file:
     content = file.read()
     s3Config = load(stream = content, Loader = Loader)["s3"]
+    dateTimeConfig = load(stream = content, Loader = Loader)["dateTime"] 
 
 
 class S3(object) :
@@ -25,6 +26,9 @@ class S3(object) :
         self.bucketName = s3Config["bucketName"]
         self.use_ssl = s3Config["use_ssl"]
         self.verify = s3Config["verify"]
+        self.zone = dateTimeConfig["zone"]
+        self.minutesOlder = dateTimeConfig["minutesOlder"]
+        self.dryRun = dateTimeConfig["dryRun"]
 
         self.s3Config = Config(signature_version = self.signature_version,
                                s3={'addressing_style': self.addressing_style},
@@ -52,10 +56,24 @@ class S3(object) :
         _result = self.client.list_buckets()
         print(_result)
 
+    def get_mpart_uploads(self):
+        _result = self.client.list_multipart_uploads(Bucket=self.bucketName)
+
+        try:
+            self.uploads = _result['Uploads']
+        except Exception as err:
+            self.uploads = None
+
     def list_mpart_uploads(self):
         _result = self.client.list_multipart_uploads(Bucket=self.bucketName)
+
+        try:
+            self.uploads = _result['Uploads']
+        except Exception as err:
+            self.uploads = None
+
         print(" List Multi-Part Uploads ".center(100, "="))
-        pprint.pprint(_result)
+        pprint.pprint(self.uploads if self.uploads else "Empty")
 
     def create_multipart_upload(self, key = None):
         _response = self.client.create_multipart_upload(
@@ -88,6 +106,7 @@ class S3(object) :
                 Bucket = self.bucketName,
                 Key = key,
                 UploadId = uploadId,
+                MaxParts= 1000
             )
         
         print(f" Multi-Part Upload Parts for {uploadId} ".center(100, "="))
@@ -95,31 +114,119 @@ class S3(object) :
 
     def abort_multipart_upload(self, key = None, uploadId = None):
 
-        _response = self.client.abort_multipart_upload(
+        print(" Multi-Part Upload Aborted ".center(100, "="))
+        try:
+            _response = self.client.abort_multipart_upload(
+                            Bucket = self.bucketName,
+                            Key= key,
+                            UploadId= uploadId,
+                        )
+            
+            if(int(_response['ResponseMetadata']['HTTPStatusCode']) >= 200 and int(_response['ResponseMetadata']['HTTPStatusCode']) < 300):
+                print(f"Successfully aborted {uploadId} for {key}!")
+            else:
+                print(f"Abort failed for {uploadId} for {key}!")
+
+        except Exception as err:
+            print(err)
+    
+    def abort_multipart_uploads(self, uploads = None, minutesOlder = None, zone = None, dryRun = False):
+
+        # Precedence over method supplied parameters over object properties. Object properties are read from config files. 
+        uploads = uploads or self.uploads
+        minutesOlder = minutesOlder or self.minutesOlder
+        zone = zone or self.zone
+        dryRun = dryRun or self.dryRun
+
+        uploadIdsToAbort = []
+
+        print(f" List of Multi-Part Uploads Older Than {minutesOlder} Minutes To Abort ".center(100, "="))
+        if not uploads:
+            pprint.pprint("Empty")
+            return False
+        
+        for upload in uploads:
+            uploadCreateDateTime = upload["Initiated"]
+            # print(upload["Initiated"])
+            # print(upload["Initiated"].tzinfo)
+
+            if zone:
+                try:
+                    # print(timeZone)
+                    timeZone = pytz.timezone(zone = zone)
+                    dateTime = uploadCreateDateTime.astimezone(timeZone)
+                    if dateTime < timeZone.localize(datetime.datetime.now()) - datetime.timedelta(minutes = minutesOlder):
+                        uploadIdsToAbort.append({upload['Key']:[upload['UploadId'], dateTime]})
+
+                except Exception as err:
+                    print("Timezone is invalid! Omitting the timezone...")
+                    print(err)
+            else:
+                
+                if uploadCreateDateTime < datetime.datetime.now() - datetime.timedelta(minutes = minutesOlder):
+                    uploadIdsToAbort.append({upload['Key']:[upload['UploadId'], uploadCreateDateTime]})
+                
+        self.uploadIdsToAbort = None or uploadIdsToAbort
+        
+        pprint.pprint(self.uploadIdsToAbort)
+
+        ################# Actual Aborts! ##############
+        print(" Multi-Part Uploads Aborted ".center(100, "="))
+
+        if dryRun:
+            print("DryRun is set. No uploads are aborted...")
+            return True
+
+        if not self.uploadIdsToAbort:
+            print("No matching uploads found. quiting..")
+            return True
+
+        for mpu in self.uploadIdsToAbort:
+            key = None
+            uploadId = None
+            date = None
+            for key, [uploadId, date] in mpu.items():
+                print(f"Aborting upload Id: {uploadId} for key: {key} created on {date}...".ljust(60, '-'))
+
+                try:
+                    _response = self.client.abort_multipart_upload(
                         Bucket = self.bucketName,
                         Key= key,
-                        UploadId= uploadId,
+                        UploadId= uploadId
                     )
-        
-        print(" Multi-Part Upload Deleted ".center(100, "="))
-        pprint.pprint(_response)
+
+                    if(int(_response['ResponseMetadata']['HTTPStatusCode']) >= 200 and int(_response['ResponseMetadata']['HTTPStatusCode']) < 300):
+                        print(f"Successfully aborted!")
+                    else:
+                        print(f"Abort failed!")
+                except Exception as err:
+                    print(err)
+
 
 
 if __name__ == "__main__":
 
-    #################### Variables ############################################
+    ###########################################################################
+    #################### Optional Variables ############################################
     key = 'mpart_obj1'
-    uploadId = '1_1000000018001_5' # Change to a real uploadId
+    uploadId = '1_1000000018001_e' # Change to a real uploadId
     part = random.randbytes(1024) # This creates a 1KB part, change as desired
-    partNumber = 1 # Change (or dont) with every part upload. If you dont designated part is overwritten.
+    partNumber = 1 # Change (or dont) with every part upload. If you dont, designated part is overwritten if it is uploaded earlier.
+    zone = None # When specified overwrites the value from the config file. Set it to None to render it ineffective. For a full list of valid timezones see the output for pprint.pprint(pytz.all_timezones) 
+    minutesOlder = None      # When specified overwrites the value from the config file. Set it to None to render it ineffective.
+    uploads = None      # When specified overwrites the value from the config file. Set it to None to render it ineffective.
+    ############################################################################
     ############################################################################
 
     s3 = S3(s3Config = s3Config)
     s3.connect()
     # s3.list_buckets()
+    s3.get_mpart_uploads() # This should always be on
+    # s3.list_mpart_uploads()
     # s3.create_multipart_upload(key = key)
     # s3.abort_multipart_upload(key = key, uploadId = uploadId)
-    # s3.list_mpart_uploads()
     # s3.upload_part(key = key, uploadId = uploadId, part = part, partNumber = partNumber)
     # s3.list_parts(key = key, uploadId = uploadId)
+    # s3.abort_multipart_uploads(uploads = uploads, minutesOlder = minutesOlder, zone = zone) # Run this method after list_mpart_uploads() or provide a custom list of uploads
+
     
